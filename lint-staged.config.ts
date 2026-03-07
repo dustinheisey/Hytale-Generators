7a26ab28a9e4f0dc1f528d8ed64bc1f9ae9e8d51
Dustin Heisey <dustin@heisey.email>
2026-03-07 13:43:45 +0100

    fix: some issues with husky

--- lint-staged.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Configuration } from "lint-staged";
-
-export default {
-  "*.{js,ts,json}": "cspell --no-must-find-files",
-  "**/*": "npm run format",
-  "**/*.ts": "npm run lint:js"
-} satisfies Configuration;
